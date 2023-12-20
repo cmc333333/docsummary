45a3bf5ae44c660412c4590cee353f104c5903d2
CM Lubinski <cm.lubinski@gmail.com>
2023-12-20 21:25:27 +0000

    Add asset bucket, custom role.
    These were created by hand previously.

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,38 @@ terraform {
 
 locals {
   project_id = "cm-docsummary"
-  region = "us-west1"
+  region     = "us-west1"
 }
 
 provider "google" {
   project                     = local.project_id
   impersonate_service_account = "deployer@${local.project_id}.iam.gserviceaccount.com"
   region                      = local.region
+}
+
+resource "google_project_service" "storage" {
+  service = "storage.googleapis.com"
+}
+
+resource "google_project_service" "generativelanguage" {
+  service = "generativelanguage.googleapis.com"
+}
+
+resource "google_storage_bucket" "assets" {
+  project                     = google_project_service.storage.project
+  name                        = "${local.project_id}-assets"
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "public_assets" {
+  bucket = google_storage_bucket.assets.name
+  role   = "roles/storage.legacyObjectReader"
+  member = "allUsers"
+}
+
+resource "google_project_iam_custom_role" "users" {
+  role_id     = "docsummaryUser"
+  title       = "DocSummary User"
+  permissions = ["aiplatform.endpoints.predict"]
 }
