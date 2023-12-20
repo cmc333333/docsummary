d46ca83f8032bc8cde80edc8d68404e2635ab718
CM Lubinski <cm.lubinski@gmail.com>
2023-12-20 21:11:56 +0000

    Bootstrap terraform.

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  backend "local" {}
+
+  required_providers {
+    google = "~> 5.9.0"
+  }
+}
+
+locals {
+  project_id = "cm-docsummary"
+}
+
+provider "google" {
+  project = local.project_id
+}
+
+resource "google_project" "project" {
+  name       = "DocSummary"
+  project_id = local.project_id
+}
+
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+}
+
+resource "google_service_account" "deployer" {
+  account_id = "deployer"
+  project    = google_project_service.iam.project
+}
+
+resource "google_service_account_iam_binding" "deployer_impersonators" {
+  service_account_id = google_service_account.deployer.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  members            = ["user:cmc333333@gmail.com"]
+}
+
+resource "google_project_iam_member" "deployer" {
+  project = google_project.project.project_id
+  role    = "roles/owner"
+  member  = google_service_account.deployer.member
+}
+
+resource "google_project_service" "storage" {
+  service = "storage.googleapis.com"
+}
+
+resource "google_storage_bucket" "deployment_state" {
+  project                     = google_project_service.storage.project
+  name                        = "${local.project_id}-deployment-state"
+  location                    = "US"
+  uniform_bucket_level_access = true
+  versioning {
+    enabled = true
+  }
+  # Delete old versions after a year
+  lifecycle_rule {
+    condition {
+      age        = 365
+      with_state = "ARCHIVED"
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
